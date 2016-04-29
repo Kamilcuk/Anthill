36e5d010e8b93641b7270c9432662b941f1512c3
msypetkowski <M.Sypetkowski@gmail.com>
2016-04-29 10:21:30 +0200

    trying again

--- simulation/controller.hpp
+++ simulation/controller.hpp
@@ -1,6 +1,8 @@
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
 
+#include "serialization.hpp"
+
 class Creature;
 
 class Controller{
@@ -11,12 +13,27 @@ public:
         owner_=owner;
     }
     virtual void step(int deltaTime)=0;
+
+private:
+	friend class boost::serialization::access;
+	template<class Archive>
+	void serialize(Archive& ar, const unsigned int version)
+    {
+	}
 };
 
 class AntWorkerAI : public Controller{
 public:
     AntWorkerAI(Creature* owner);
     void step(int deltaTime);
+
+private:
+	friend class boost::serialization::access;
+	template<class Archive>
+	void serialize(Archive& ar, const unsigned int version)
+    {
+        ar & boost::serialization::base_object<Controller>(*this);
+	}
 };
 
 #endif
