106e24191f6efc43ef154eaf7c4cc0af01d682e4
m-kostrzewa <kostrzewa.michal@o2.pl>
2016-05-01 18:23:40 +0200

    constexpr in Point class

--- simulation/point.cpp
+++ simulation/point.cpp
@@ -2,56 +2,14 @@
 #include <iostream>
 #include <cmath>
 
-int Point::posX() const
-{
-	return posX_;
-}
-
 void Point::setPosX(int posX)
 {
 	posX_ = posX;
 }
 
-int Point::posY() const
-{
-	return posY_;
-}
-
 void Point::setPosY(int posY)
 {
 	posY_ = posY;
-}
-
-void Point::printPosition()
-{
-	std::cout << "PosX: " << posX() << " posY: " << posY() << std::endl;
-}
-
-Point Point::getPosition() const
-{
-    return Point(*this);
-}
-
-float Point::getDistance(Point p) const
-{
-        return std::sqrt(
-                std::pow(this->posX() - p.posX(), 2) +
-                std::pow(this->posY() - p.posY(), 2)
-                );
-
-}
-
-Point::Point(int posX, int posY) :
-	posX_(posX),
-	posY_(posY)
-{
-
-}
-
-Point::Point(const Point &pos)
-{
-	posX_ = pos.posX();
-	posY_ = pos.posY();
 }
 
 std::string Point::toString() const
--- simulation/point.hpp
+++ simulation/point.hpp
@@ -11,37 +11,39 @@ class Point
 	int posX_;
 	int posY_;
 	friend std::ostream& operator<<(std::ostream &os, const Point& rhs);
+
 public:
-    Point():posX_(0),posY_(0){}
-	Point(int posX, int posY);
-	Point(const Point& pos);
+	constexpr Point() : posX_(0), posY_(0) {}
+	constexpr Point(int posX, int posY) : posX_(posX), posY_(posY) {}
+	constexpr Point(const Point &other) : posX_(other.posX_), 
+		posY_(other.posY_) {}
 	
     inline Point& operator=(const Point& rhs)
 	{ posX_ = rhs.posX_; posY_ = rhs.posY_; return *this; }
 	
-	inline bool operator<(const Point& rhs) const // for std::set
+	constexpr inline bool operator<(const Point& rhs) const // for std::set
 	{ return (posX() < rhs.posX()) || (posY() < rhs.posY()); } 
 	
-	inline bool operator>(const Point& rhs) const // for std::set
+	constexpr inline bool operator>(const Point& rhs) const // for std::set
 	{ return (posX() > rhs.posX()) || (posY() > rhs.posY()); } 
 	
-	inline bool operator!=(const Point& rhs) const 
+	constexpr inline bool operator!=(const Point& rhs) const 
 	{ return (posX() != rhs.posX()) || (posY() != rhs.posY()); } 
 	
-	inline bool operator==(const Point& rhs) const 
+	constexpr inline bool operator==(const Point& rhs) const 
 	{ return posX_ == rhs.posX_ && posY_ == rhs.posY_; }
 	
-	inline Point operator+(const Point& rhs) const 
+	constexpr inline Point operator+(const Point& rhs) const 
 	{ return Point(posX_ + rhs.posX_, posY_ + rhs.posY_); }
 	
-	inline Point operator-(const Point& rhs) const 
+	constexpr inline Point operator-(const Point& rhs) const 
 	{ return Point(posX_ - rhs.posX_, posY_ - rhs.posY_); }
 	
 	// dot product
-	inline float operator*(const Point& rhs) 
+	constexpr inline float operator*(const Point& rhs) 
 	{ return posX_ * rhs.posX_ + posY_ * rhs.posY_; }
 	
-	inline Point operator*(const float& rhs) 
+	constexpr inline Point operator*(const float& rhs) 
 	{ return Point(posX_ * rhs, posY_ * rhs); }
 	
 	inline Point& operator+=(const Point& rhs) 
@@ -55,18 +57,23 @@ public:
 	
 	std::string toString() const;
 	
-	int posX() const;
-	int posY() const;
+	constexpr inline int posX() const
+	{ return posX_; }
+	constexpr inline int posY() const
+	{ return posY_; }
 	
 	void setPosX(int posX);
 	void setPosY(int posY);
 	
-	void printPosition();
+    constexpr inline float getDistance(Point p) const
+	{
+		return std::sqrt(
+			std::pow(this->posX() - p.posX(), 2) +
+			std::pow(this->posY() - p.posY(), 2)
+			);
+	}
 	
-	Point getPosition() const;
-    float getDistance(Point p) const;
-	
-    inline bool isInBounds(int size_x, int size_y)
+    constexpr inline bool isInBounds(int size_x, int size_y) const
     {
         return posX() >= 0 && posX() < size_x && posY() >= 0 && posY() < size_y;
     }
