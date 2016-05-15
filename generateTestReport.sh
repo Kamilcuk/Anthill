6f163748446f964a54c0b78e1c113355b2452806
m-kostrzewa <kostrzewa.michal@o2.pl>
2016-05-15 18:01:58 +0200

    Fixed inline functions not showing up on test coverage

--- generateTestReport.sh
+++ generateTestReport.sh
@@ -4,7 +4,7 @@
 # paragraph 4
 
 mkdir -p test_results/
-lcov --directory simulation/ --capture --output-file test_results/report.info --no-external
-lcov --directory simulation/ --zerocounters
-genhtml -o test_results/ test_results/report.info
+lcov -d simulation/ -d _build_test/ -c -o test_results/profile_test.info -q --no-external 
+lcov -d simulation/ -d _build_test/ --zerocounters
+genhtml -o test_results/ test_results/profile_test.info -q
 see test_results/index.html
